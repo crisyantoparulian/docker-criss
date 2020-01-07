800f5af418dd8894416a3f0126ccb0ca3db2808d
crisyanto <crisyanto.p@gmail.com>
2020-01-07 17:39:35 +0700

    feature

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         HII WORLD ???? !
+         FEATURE ???? !
         </p>
         <a
           className="App-link"
